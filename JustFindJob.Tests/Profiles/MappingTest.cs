d063bd5c13ad037e8269d90c78950d18d5e0e659
MichalBoczula <michal.boczula0101@gmail.com>
2021-06-12 02:59:54 +0200

    Test project created, and queries  tests arranged

---
+++ JustFindJob.Tests/Profiles/MappingTest.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace JustFindJob.Tests.Profiles
+{
+    public class MappingTest : IClassFixture<MappingTestFixture>
+    {
+        private readonly IConfigurationProvider _configuration;
+        private readonly IMapper _mapper;
+
+        public MappingTest(MappingTestFixture fixture)
+        {
+            _configuration = fixture.Configuration;
+            _mapper = fixture.Mapper;
+        }
+
+    }
+}
